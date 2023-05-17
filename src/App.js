c648181ed0b071c9b2cb617d5b2b1bffc2605c60
ianchan3 <ianchan3138@gmail.com>
2023-05-17 15:26:14 -0700

    Select Functionality Between Hotels, Restaurants, and Attractions Implemented. Rating Filter Feature Implemented

--- src/App.js
+++ src/App.js
@@ -11,10 +11,13 @@ import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 export default function App () {
   
   const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
   const [childClicked, setChildClicked] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
 
 
 useEffect(() => {
@@ -24,24 +27,30 @@ useEffect(() => {
 }, []);
 
 useEffect(() => {
+  const filteredPlaces = places?.filter((place) => place.rating > rating);
+  setFilteredPlaces(filteredPlaces)
+}, [rating])
+
+useEffect(() => {
   setIsLoading(true);
-  getPlacesData(bounds.sw, bounds.ne)
+  getPlacesData(type, bounds.sw, bounds.ne)
   .then((data) => {
     setPlaces(data);
+    setFilteredPlaces([]);
     setIsLoading(false);
   })
-}, [coordinates, bounds]);
-// coordinates, bounds
+}, [type, coordinates, bounds]);
+// type, coordinates, bounds
   return (
     <div>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places} childClicked={childClicked} isLoading={isLoading} />
+          <List places={filteredPlaces?.length ? filteredPlaces : places} childClicked={childClicked} isLoading={isLoading} type={type} setType={setType} rating={rating} setRating={setRating} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} places={places} setChildClicked={setChildClicked} />
+          <Map setCoordinates={setCoordinates} setBounds={setBounds} coordinates={coordinates} places={filteredPlaces?.length ? filteredPlaces : places} setChildClicked={setChildClicked} />
         </Grid>
       </Grid>
     </div>
